e630ad293892df34069fbb3292993d79e8dc7acf
heysebas <sebazgrisales@gmail.com>
2025-09-01 09:03:40 -0500

    Actualizacion

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // config.js
 window.config = {
     microservicios: {
-        funcionariosService: 'http://localhost:8080', // Base URL de funcionarios y gestión
+        // funcionariosService: 'http://localhost:8080', // Base URL de funcionarios y gestión
         // informesService: 'http://localhost:8080', // Base URL de control de certificados de RTM
         // informesService: 'https://api-informes-rtm-production.up.railway.app', // Base URL de control de certificados de RTM
-        // funcionariosService: 'https://api-funcionarios-admin-migracion-production.up.railway.app', // Base URL de funcionarios y gestión
+        funcionariosService:  'https://radical-extends-assure-supplied.trycloudflare.com', // Base URL de funcionarios y gestión
         // funcionariosService: 'https://painted-conservative-alexandria-liable.trycloudflare.com',
     },
 
